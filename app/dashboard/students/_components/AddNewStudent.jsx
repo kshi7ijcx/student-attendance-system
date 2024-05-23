2edbfeaa806d929267388e44b75201e9baebb0a6
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-05-23 22:48:58 +0530

    add new student section started

---
+++ app/dashboard/students/_components/AddNewStudent.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+
+const AddNewStudent = () => {
+  return (
+    <div>
+        <Button>+ Add New Student</Button>
+    </div>
+  )
+}
+export default AddNewStudent
---
+++ app/dashboard/students/page.jsx
@@ -0,0 +1,13 @@
+import AddNewStudent from "./_components/AddNewStudent";
+
+const page = () => {
+  return (
+    <div className="p-7">
+      <h2 className="font-bold text-2xl flex justify-between items-center">
+        Students
+        <AddNewStudent />
+      </h2>
+    </div>
+  );
+};
+export default page;
