5921f1fa32dddb00e8ef29ea9451ef7219db478d
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-05-30 22:56:23 +0530

    get all students route created

--- app/_services/globalAPIs.js
+++ app/_services/globalAPIs.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
 export const getAllGrades = () => axios.get("/api/grades");
-export const addStudent = (data) => axios.post("/api/student",data);
+export const addStudent = (data) => axios.post("/api/student", data);
+export const getAllStudents = () => axios.get("/api/students");
--- app/api/student/route.js
+++ app/api/student/route.js
@@ -1,15 +1,21 @@
 import db from "@/lib";
 import { students } from "@/lib/schema";
+import { sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   const data = await req.json();
   const result = await db.insert(students).values({
-    name:data.name,
-    grade:data.grade,
-    address:data.address,
-    contact:data.contact
+    name: data.name,
+    grade: data.grade,
+    address: data.address,
+    contact: data.contact,
   });
 
+  return NextResponse.json(result);
+}
+
+export async function GET(req) {
+  const result = db.execute(sql`select * from ${students}`);
   return NextResponse.json(result);
 }
